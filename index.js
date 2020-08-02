2fedcc7573b632310ceb7d1cc382b887922c6774
brian <riceb53@gmail.com>
2020-08-02 10:48:15 -0400

    add basic vue + event listener

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('hello');
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    greeting: "Hello from Buffalo!",
+    name: "Jessica"
+  },
+  methods: {
+    changeGreeting: function() {
+      console.log('changing greeting...');
+      console.log(this.$data);
+      this.greeting = "goodbye from toronto";
+    }
+  }
+});
